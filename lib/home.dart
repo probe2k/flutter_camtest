b05b6fc7402840c4aee772703499c0efd4b9224c
probe2k <kyash054@gmail.com>
2020-12-10 02:08:13 +0530

    camera services add

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 class AppHome extends StatefulWidget {
   @override
@@ -6,8 +7,127 @@ class AppHome extends StatefulWidget {
 }
 
 class _AppHomeState extends State<AppHome> {
+  CameraController cameraController;
+  List camera;
+  int selectedCameraIndex;
+  String imgPath;
+
+  Future _initCameraController(CameraDescription cameraDescription) async {
+    if (cameraController != null) {
+      await cameraController.dispose();
+    }
+
+    cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    if (cameraController.value.hasError) {
+      print(
+          'Camera controller value error ${cameraController.value.errorDescription}');
+    }
+
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (exc) {
+      _showCameraException(exc);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Widget _cameraPreviewWidget() {
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return const Text(
+        'Loading',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: cameraController.value.aspectRatio,
+      child: CameraPreview(
+        cameraController,
+      ),
+    );
+  }
+
+  Widget _cameraControlWidget(context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            FloatingActionButton(
+              child: Icon(
+                Icons.camera,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.white,
+              onPressed: () {
+                _onCapturePressed(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _cameraToggleRowWidget() {
+    if (camera == null || camera.isEmpty) {
+      return Spacer();
+    }
+
+    CameraDescription selectedCamera = camera[selectedCameraIndex];
+    CameraLensDirection lensDirection = selectedCamera.lensDirection;
+
+    return Expanded(
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: FlatButton.icon(
+          onPressed: _onSwitchCamera,
+          icon: Icon(
+            _getCameraLensIcon(lensDirected),
+            color: Colors.white,
+            size: 24,
+          ),
+          label: Text(
+            '${lensDirection.toString().substring(lensDirection.toString().indexOf('.') + 1).toUpperCase()}',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return null;
   }
+
+  void _showCameraException(CameraException exc) {
+    String errorText =
+        'Error : ${exc.code}\n ERROR MESSAGE: ${exc.description}';
+    print(errorText);
+  }
+
+  void _onCapturePressed(context) {}
+
+  void _onSwitchCamera() {}
 }
