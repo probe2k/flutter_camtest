6aafeee06b1120a955771890a2e3716bdfab78a9
probe2k <kyash054@gmail.com>
2020-12-21 18:56:50 +0530

    Media Share viewerState fix

--- lib/pages/view.dart
+++ lib/pages/view.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class Viewer extends StatefulWidget {
   String imgPath;
@@ -15,7 +17,43 @@ class Viewer extends StatefulWidget {
 class _ViewerState extends State<Viewer> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Image.file(File(widget.imgPath), fit: BoxFit.cover),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                height: 60,
+                color: Colors.black,
+                child: Center(
+                  child: IconButton(
+                      icon: Icon(
+                        Icons.share,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        getBytes().then((bytes) {
+                          Share.file('Share via', basename(widget.imgPath),
+                              bytes.buffer.asUint8List(), 'image/path');
+                        });
+                      }),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Future<ByteData> getBytes() async {
