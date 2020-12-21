a3f951d5cf55e7a0277e1bd86af5ce59ffe473c1
probe2k <kyash054@gmail.com>
2020-12-21 19:03:55 +0530

    SystemUIElements appearance handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:camera_flutter/home.dart';
+import 'package:flutter/services.dart';
 
 void run() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.black,
+      ),
+    );
+
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
       home: AppHome(),
       debugShowCheckedModeBanner: false,
     );
