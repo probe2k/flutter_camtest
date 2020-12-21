f152127cfbf997f8db2aabbda99035e3544b468d
probe2k <kyash054@gmail.com>
2020-12-21 18:40:57 +0530

    viewer BPC config add

---
+++ lib/pages/view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Viewer extends StatefulWidget {
+  String imgPath;
+
+  Viewer({this.imgPath});
+
+  @override
+  _ViewerState createState() => _ViewerState();
+}
+
+class _ViewerState extends State<Viewer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
