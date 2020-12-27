3a207a92b3be23f262e3d2b42acc412945f5b5bc
probe2k <kyash054@gmail.com>
2020-12-27 18:47:53 +0530

    Init commit -> TODO: ListViewBuilder staging

---
+++ lib/pages/settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CameraSettings extends StatefulWidget {
+  _CameraSettingsState createState() => _CameraSettingsState();
+}
+
+class _CameraSettingsState extends State<CameraSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
